ed695b04cd673f3ff76510826300deaa4b5344f2
NanezX <victor.nanezj@gmail.com>
2023-11-15 05:37:42 -0400

    export json string class

--- assembly/index.ts
+++ assembly/index.ts
@@ -5,7 +5,9 @@ import {
   isHexString,
 } from "./hexadecimal";
 import { hexToBigInt, getZeros } from "./numbers";
+import { JsonString } from "./json_string";
 
 export { MagicNumbers };
 export { getEvenHexString, hexStringToArrayBuffer, isHexString };
 export { hexToBigInt, getZeros };
+export { JsonString };
