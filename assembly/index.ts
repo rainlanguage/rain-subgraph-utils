cb06839113e7ed205a8ad82fd9339d0ebd363983
NanezX <victor.nanezj@gmail.com>
2023-11-15 08:08:54 -0400

    constant tuple prefix string

--- assembly/index.ts
+++ assembly/index.ts
@@ -6,8 +6,10 @@ import {
 } from "./hexadecimal";
 import { hexToBigInt, getZeros } from "./numbers";
 import { JsonString } from "./json_string";
+import { tuplePrefixString } from "./constants";
 
 export { MagicNumbers };
 export { getEvenHexString, hexStringToArrayBuffer, isHexString };
 export { hexToBigInt, getZeros };
 export { JsonString };
+export { tuplePrefixString };
